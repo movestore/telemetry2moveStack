20471dc34912ad0368a010b0c51f2de54f0e2df5
annescharf <ascharf@ab.mpg.de>
2023-03-01 21:21:02 +0100

    cleanup

--- RFunction.R
+++ RFunction.R
@@ -4,10 +4,6 @@ library("ctmm")
 # input: telemetry.list - output: movestack
 
 rFunction <- function(data){
-  # incase we would need to do some checking for the input
-  # if(class(data)== "telemetry"){
-  #   data <- list(data)
-  # } else if((unique(unlist(lapply(data, class))) == "telemetry")){data <- data}
-  move(data) # when input is a list of telemetry obj, output is a movestack. if input is a telemetry obj, output is move
+  move(data) 
 }
 
